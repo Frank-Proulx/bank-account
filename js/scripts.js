74e974010365acd2a178379bb83eb2fc06ca53d7
Frank-Proulx <frankproulx7@gmail.com>
2021-11-02 14:44:25 -0700

    add css styling to page
    Co-authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,13 @@ BankAccount.prototype.withdraw = function(amount) {
   this.balance -= amount;
 };
 
-
-
-
-
 $(document).ready(function() {
   $("form#register").submit(function(event) {
     event.preventDefault();
     let name = $("#name").val();
     let balance = parseFloat($("#initial-deposit").val());
     account1 = new BankAccount(name, balance);
-    $("#balance").html(account1.balance);
+    $("#balance").html("$ " + account1.balance);
     $("#name").val("");
     $("#initial-deposit").val("");
     $(".bank").show();
@@ -35,7 +31,7 @@ $(document).ready(function() {
     let withdrawal = parseFloat($("#withdrawal-amount").val());
     account1.deposit(deposit);
     account1.withdraw(withdrawal);
-    $("#balance").html(account1.balance);
+    $("#balance").html("$ " + account1.balance);
     $("#deposit-amount").val("");
     $("#withdrawal-amount").val("");
   });
